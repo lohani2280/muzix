5dd39cd534df0a3a74ea1ad5a1b53b09062a3d71
lohani2280 <lohani.ayush01@gmail.com>
2019-10-15 15:26:37 +0530

    used lombok

--- src/main/java/com/stackroute/muzix/domain/Track.java
+++ src/main/java/com/stackroute/muzix/domain/Track.java
@@ -1,56 +1,25 @@
 package com.stackroute.muzix.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@Data //Using lombok to handle the getter setters
+@NoArgsConstructor //Creates a zero argument constructor
+@AllArgsConstructor //Creates an all argument constructor
+//@Builder
+
 public class Track {
     @Id
 //    @GeneratedValue(strategy = GenerationType.AUTO)
     private int trackID;
     private String trackName;
     private String comments;
-
-    public Track(int trackID, String trackName, String comments) {
-        this.trackID = trackID;
-        this.trackName = trackName;
-        this.comments = comments;
-    }
-
-    public Track() {
-    }
-
-    public int getTrackID() {
-        return trackID;
-    }
-
-    public void setTrackID(int trackID) {
-        this.trackID = trackID;
-    }
-
-    public String getTrackName() {
-        return trackName;
-    }
-
-    public void setTrackName(String trackName) {
-        this.trackName = trackName;
-    }
-
-    public String getComments() {
-        return comments;
-    }
-
-    public void setComments(String comments) {
-        this.comments = comments;
-    }
-    @Override
-    public String toString() {
-        return "Track{" +
-                "trackID=" + trackID +
-                ", trackName='" + trackName + '\'' +
-                ", comments='" + comments + '\'' +
-                '}';
-    }
 }
